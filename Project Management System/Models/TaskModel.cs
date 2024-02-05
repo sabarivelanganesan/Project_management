3bc97dc40f90180517f8978baf37bd00aced9311
Sabarivelan Ganesan <sabarivelangn@gmail.com>
2024-02-05 21:31:48 +0000

    [Feature] Task Modules

--- Project Management System/Models/TaskModel.cs
+++ Project Management System/Models/TaskModel.cs
@@ -16,6 +16,9 @@
 
         public int ProjectId { get; set; }
         public string TaskName { get; set; }
+        public string TaskOwner { get; set; }
+
+        public string TaskStatus { get; set; }
         public TimeSpan TaskDuration { get; set; }
 
         public string TaskDescription { get; set; }
