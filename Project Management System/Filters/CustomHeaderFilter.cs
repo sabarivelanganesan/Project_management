92043adcd9ced9cfaa98c02d1b5f557be95627c7
Sabarivelan Ganesan <sabarivelangn@gmail.com>
2024-02-06 10:51:52 +0000

    [Feature] Action Filters

---
+++ Project Management System/Filters/CustomHeaderFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Project_Management_System.Filters
+{
+    public class CustomHeaderFilter : IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add("X-myproject-header", "ProjectManagement");
+        }
+    }
+}
---
+++ Project Management System/Filters/LogActionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Project_Management_System.Filters
+{
+    public class LogActionFilter : IActionFilter
+    {
+        private readonly ILogger<LogActionFilter> _logger;
+
+        public LogActionFilter(ILogger<LogActionFilter> logger) {  _logger = logger; }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation($"Action '{context.ActionDescriptor.DisplayName}' has completed.");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation($"Action '{context.ActionDescriptor.DisplayName}' is starting.");
+        }
+    }
+}
